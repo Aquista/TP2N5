60af869e33bf2384dc79e523209e7014d035cda6
Aquista <kaquista@fi.uba.ar>
2020-07-29 13:06:34 -0300

    fix: Arreglo el calcularPuntaje en PuntajeClasico, cambio Interfaz IPregunta por clase abstracta Pregunta

--- src/main/java/edu/fiuba/algo3/modelo/Puntajes/Puntaje.java
+++ src/main/java/edu/fiuba/algo3/modelo/Puntajes/Puntaje.java
@@ -6,7 +6,6 @@ public abstract class IPuntaje {
     protected int puntos = 0;
     protected int cantidadCorrectas = 0;
     protected int cantidadIncorrectas = 0;
-    abstract void calcularPuntaje( Jugador unJugador);
     
     public void sumarCorrecta() {
         cantidadCorrectas += 1;
