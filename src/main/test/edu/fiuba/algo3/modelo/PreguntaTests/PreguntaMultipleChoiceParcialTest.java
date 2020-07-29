1f8a487e501d3718f1684610eddaef94aed972b5
federico <ftorcini@fi.uba.ar>
2020-07-29 17:29:24 -0300

    Feat: Completo tests MultipleChoiceParcial.

--- src/main/test/edu/fiuba/algo3/modelo/PreguntaTests/PreguntaMultipleChoiceParcialTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PreguntaTests/PreguntaMultipleChoiceParcialTest.java
@@ -68,4 +68,59 @@ public class PreguntaMultipleChoiceParcialTest {
 
         assertEquals(puntajeEsperado, jugador.getPuntaje());
     }
+
+    @Test
+    public void test03CreoUnaPreguntaUnJugadorCon10PuntosQueContesta2OpcionesMalTerminaCon10Puntos(){
+        int puntajeInicial = 10;
+        int puntajeEsperado = 10;
+
+        Jugador jugador = new Jugador();
+        jugador.actualizarPuntaje(puntajeInicial);
+        OpcionCorrecta op1 = new OpcionCorrecta("texto opcion 1");
+        OpcionIncorrecta op2 = new OpcionIncorrecta("texto opcion 2");
+        OpcionIncorrecta op3 = new OpcionIncorrecta("texto opcion 3");
+        PreguntaMultipleChoiceParcial pregunta = new PreguntaMultipleChoiceParcial("Soy unaa pregunta");
+        pregunta.agregarOpcionCorrecta(op1);
+        pregunta.agregarOpcionIncorrecta(op2);
+        pregunta.agregarOpcionIncorrecta(op3);
+
+        RespuestaMultipleChoice resp = new RespuestaMultipleChoice();
+        resp.agregarOpcion(op1);
+        resp.agregarOpcion(op2);
+        jugador.establecerRespuesta(resp);
+
+        ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+        jugadores.add(jugador);
+
+        pregunta.evaluarJugadores(jugadores);
+
+        assertEquals(puntajeEsperado, jugador.getPuntaje());
+    }
+
+    @Test
+    public void test04CreoUnaPreguntaConDosOpcionesCorrectasUnJugadorCon10PuntosQueContesta1OpcionBienTerminaCon11Puntos(){
+        int puntajeInicial = 10;
+        int puntajeEsperado = 11;
+
+        Jugador jugador = new Jugador();
+        jugador.actualizarPuntaje(puntajeInicial);
+        OpcionCorrecta op1 = new OpcionCorrecta("texto opcion 1");
+        OpcionCorrecta op2 = new OpcionCorrecta("texto opcion 2");
+        OpcionIncorrecta op3 = new OpcionIncorrecta("texto opcion 3");
+        PreguntaMultipleChoiceParcial pregunta = new PreguntaMultipleChoiceParcial("Soy unaa pregunta");
+        pregunta.agregarOpcionCorrecta(op1);
+        pregunta.agregarOpcionCorrecta(op2);
+        pregunta.agregarOpcionIncorrecta(op3);
+
+        RespuestaMultipleChoice resp = new RespuestaMultipleChoice();
+        resp.agregarOpcion(op1);
+        jugador.establecerRespuesta(resp);
+
+        ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+        jugadores.add(jugador);
+
+        pregunta.evaluarJugadores(jugadores);
+
+        assertEquals(puntajeEsperado, jugador.getPuntaje());
+    }
 }
