e3cd096840c9cf55d04e31c26b0da5514358dbf2
Aquista <kaquista@fi.uba.ar>
2020-07-29 12:54:10 -0300

    refactor: Divido las pruebas de PreguntaVF en PreguntaVF y PreguntaVFConPenalidad

--- src/main/test/edu/fiuba/algo3/modelo/PreguntaTests/PreguntaVFConPenalidadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PreguntaTests/PreguntaVFConPenalidadTest.java
@@ -1,10 +1,8 @@
 package edu.fiuba.algo3.modelo.PreguntaTests;
 
-import edu.fiuba.algo3.modelo.*;
-import edu.fiuba.algo3.modelo.Opciones.Opcion;
+import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.Opciones.OpcionCorrecta;
 import edu.fiuba.algo3.modelo.Opciones.OpcionIncorrecta;
-import edu.fiuba.algo3.modelo.Preguntas.PreguntaVF;
 import edu.fiuba.algo3.modelo.Preguntas.PreguntaVFConPenalidad;
 import edu.fiuba.algo3.modelo.Respuestas.RespuestaVF;
 import org.junit.jupiter.api.Test;
@@ -13,59 +11,64 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PreguntaVFTest {
-
+public class PreguntaVFConPenalidadTest {
     @Test
-    public void test01CreoUnaPreguntaClasicaYEvaluo1JugadorCon1RespuestaCorrecta() {
+    public void test01CreoUnaPreguntaConPenalidadYEvaluo1JugadorCon1RespuestaCorrecta() {
+
         int puntajeEsperado = 1;
+        Jugador primerJugador = new Jugador();
 
         OpcionCorrecta opcionCorrecta = new OpcionCorrecta("true");
         OpcionIncorrecta opcionIncorrecta = new OpcionIncorrecta("false");
 
-        PreguntaVF pregunta = new PreguntaVF();
+        PreguntaVFConPenalidad pregunta = new PreguntaVFConPenalidad();
         pregunta.agregarOpcionCorrecta(opcionCorrecta);
         pregunta.agregarOpcionIncorrecta(opcionIncorrecta);
 
-        Jugador jugador = new Jugador();
-        RespuestaVF respuesta=new RespuestaVF();
-        respuesta.agregarOpcion(opcionCorrecta);
-        jugador.establecerRespuestas(respuesta);
-        ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
-        jugadores.add(jugador);
+        RespuestaVF respuesta1=new RespuestaVF();
+        respuesta1.agregarOpcion(opcionCorrecta);
+        primerJugador.establecerRespuestas(respuesta1);
 
+
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+        jugadores.add(primerJugador);
         pregunta.evaluarJugadores(jugadores);
 
-        assertEquals(puntajeEsperado,jugador.getPuntaje());
+        assertEquals(puntajeEsperado,primerJugador.getPuntaje());
     }
 
     @Test
-    public void test02CreoUnaPreguntaClasicaYEvaluo2Jugadores() {
+    public void test02CreoUnaPreguntaConPenalidadYEvaluo2JugadoresConRespuestaCorrectaEInconrrecta() {
+
+        int puntosGanados=4;
+        Jugador primerJugador = new Jugador();
+        Jugador segundoJugador= new Jugador();
+        primerJugador.actualizarPuntaje(puntosGanados);
+        segundoJugador.actualizarPuntaje(puntosGanados);
+
 
         OpcionCorrecta opcionCorrecta = new OpcionCorrecta("true");
         OpcionIncorrecta opcionIncorrecta = new OpcionIncorrecta("false");
 
-        PreguntaVF pregunta = new PreguntaVF();
+        PreguntaVFConPenalidad pregunta = new PreguntaVFConPenalidad();
         pregunta.agregarOpcionCorrecta(opcionCorrecta);
         pregunta.agregarOpcionIncorrecta(opcionIncorrecta);
-        
-        Jugador jugador1 = new Jugador();
+
         RespuestaVF respuesta1=new RespuestaVF();
         respuesta1.agregarOpcion(opcionCorrecta);
-        jugador1.establecerRespuestas(respuesta1);
-        
+        primerJugador.establecerRespuestas(respuesta1);
 
-        Jugador jugador2 = new Jugador();
         RespuestaVF respuesta2=new RespuestaVF();
         respuesta2.agregarOpcion(opcionIncorrecta);
-        jugador2.establecerRespuestas(respuesta2);
+        segundoJugador.establecerRespuestas(respuesta2);
 
         ArrayList<Jugador> jugadores = new ArrayList<>();
-        jugadores.add(jugador1);
-        jugadores.add(jugador2);
+        jugadores.add(primerJugador);
+        jugadores.add(segundoJugador);
 
         pregunta.evaluarJugadores(jugadores);
 
-        assertEquals(1,jugador1.getPuntaje());
-        assertEquals(0,jugador2.getPuntaje());
+        assertEquals(puntosGanados+1,primerJugador.getPuntaje());
+        assertEquals(puntosGanados-1,segundoJugador.getPuntaje());
     }
 }
