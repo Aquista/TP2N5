69764b446969ea7687678151ad16fd6c51251113
federico <ftorcini@fi.uba.ar>
2020-07-29 16:56:26 -0300

    Feat: Agregado PreguntaMultipleChoiceParcial y algunos tests correspondientes.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Preguntas/PreguntaMultipleChoiceParcial.java
@@ -0,0 +1,17 @@
+package edu.fiuba.algo3.modelo.Preguntas;
+
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.Puntajes.PuntajeParcial;
+
+import java.util.ArrayList;
+
+public class PreguntaMultipleChoiceParcial extends Pregunta{
+
+    @Override
+    public void evaluarJugadores(ArrayList<Jugador> jugadores) {
+        for(Jugador jugador : jugadores) {
+            PuntajeParcial puntajeJugador = new PuntajeParcial();
+            puntajeJugador.calcularPuntaje(jugador);
+        }
+    }
+}
